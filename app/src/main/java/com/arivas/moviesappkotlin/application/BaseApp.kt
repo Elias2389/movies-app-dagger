2dcd77e75368582f01a26c1266a6d2810fe6b3e7
Andres <andres.rivas@mercadolibre.com>
2020-02-08 15:21:29 -0300

    refactor

--- app/src/main/java/com/arivas/moviesappkotlin/application/BaseApp.kt
+++ app/src/main/java/com/arivas/moviesappkotlin/application/BaseApp.kt
@@ -13,7 +13,6 @@ class BaseApp: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        //Initialize Fresco
         Fresco.initialize(this)
 
         component = DaggerApplicationComponent

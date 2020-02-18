e7568ecb4e6ae2feec29836b266a86d4a9822c75
Andres <andres.rivas@mercadolibre.com>
2020-02-18 14:13:09 -0300

    Added test for get data

--- app/src/test/java/com/arivas/moviesappkotlin/ui/movies/viewmodel/MoviesViewModelTest.kt
+++ app/src/test/java/com/arivas/moviesappkotlin/ui/movies/viewmodel/MoviesViewModelTest.kt
@@ -1,6 +1,8 @@
 package com.arivas.moviesappkotlin.ui.movies.viewmodel
 
-import com.arivas.moviesappkotlin.common.network.services.MoviesServices
+import androidx.lifecycle.LiveData
+import com.arivas.moviesappkotlin.common.dto.ResultsItem
+import com.arivas.moviesappkotlin.common.network.networkboundresource.Resource
 import com.arivas.moviesappkotlin.ui.movies.repository.MoviesRepository
 import org.junit.Assert
 import org.junit.Before
@@ -20,16 +22,25 @@ class MoviesViewModelTest {
     @Mock
     lateinit var moviesViewModel: MoviesViewModel
 
+    @Mock
+    lateinit var data: LiveData<Resource<List<ResultsItem>>>
+
     @Before
-    public fun setup() {
+    fun setup() {
         MockitoAnnotations.initMocks(this)
         moviesViewModel = MoviesViewModel(moviesRepository)
     }
 
     @Test
-    public fun apiShouldBeNullTest() {
+    fun apiShouldBeNullTest() {
         Mockito.`when`(moviesRepository.getPopularMovies()).thenReturn(null)
         Assert.assertNull(moviesViewModel.getPopularMovies())
+    }
+
+    @Test
+    fun apiShouldFetchDataTest() {
+        Mockito.`when`(moviesRepository.getPopularMovies()).thenReturn(data)
+        Assert.assertNotNull(moviesViewModel.getPopularMovies())
     }
 
 }

945cc9e8e9bf3620070facf5affe65ee76288493
Andres <andres.rivas@mercadolibre.com>
2020-07-19 16:16:31 -0300

    Added initial doc to interfaces

--- app/src/main/java/com/arivas/moviesappkotlin/common/db/MoviesDao.kt
+++ app/src/main/java/com/arivas/moviesappkotlin/common/db/MoviesDao.kt
@@ -4,12 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.arivas.moviesappkotlin.common.dto.ResultsItem
 
-
 @Dao
 interface MoviesDao {
+
+    /**
+     * Method to get data from DB
+     *
+     * @return results
+     */
     @Query("SELECT * FROM movies")
     fun getMoviesFromDatabase(): LiveData<List<ResultsItem>>
 
+    /**
+     * Insert results in DB
+     *
+     * @param resultsItem
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertMoviesToDatabase(resultsItem: List<ResultsItem>)
 }

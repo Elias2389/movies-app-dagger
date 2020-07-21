7a65d5118935e20bc76e4833d09e45944a2f12c4
Andres <andres.rivas@mercadolibre.com>
2020-07-21 09:38:51 -0300

    Added first request for paging

--- app/src/main/java/com/arivas/moviesappkotlin/ui/movies/paging/MoviesDataSource.kt
+++ app/src/main/java/com/arivas/moviesappkotlin/ui/movies/paging/MoviesDataSource.kt
@@ -1,13 +1,30 @@
 package com.arivas.moviesappkotlin.ui.movies.paging
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.paging.PageKeyedDataSource
+import com.arivas.moviesappkotlin.common.dto.MoviesResponse
 import com.arivas.moviesappkotlin.common.dto.ResultsItem
+import com.arivas.moviesappkotlin.common.network.services.MoviesServices
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MoviesDataSource: PageKeyedDataSource<Long, ResultsItem>() {
+    private val moviesServices: MoviesServices? = null
+    private val firstPage: Long = 1
 
+    @SuppressLint("CheckResult")
     override fun loadInitial(params: LoadInitialParams<Long>,
                              callback: LoadInitialCallback<Long, ResultsItem>) {
-        TODO("Not yet implemented")
+        getCall(1)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({ response ->
+                callback.onResult(response.results, null, firstPage + 1L)
+            },{ error ->
+                Log.e("Error:", error.message)
+            })
     }
 
     override fun loadAfter(params: LoadParams<Long>, callback: LoadCallback<Long, ResultsItem>) {
@@ -16,5 +33,9 @@ class MoviesDataSource: PageKeyedDataSource<Long, ResultsItem>() {
 
     override fun loadBefore(params: LoadParams<Long>, callback: LoadCallback<Long, ResultsItem>) {
         TODO("Not yet implemented")
+    }
+
+    private fun getCall(page: Int): Observable<MoviesResponse> {
+        return moviesServices?.getPopularMoviesByPaging(page)!!
     }
 }
